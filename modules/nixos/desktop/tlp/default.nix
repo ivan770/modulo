fe73d880ff0eb9dc99f11e158427c8f4f65703c4
ivan770 <ivan@ivan770.me>
2024-12-07 12:47:43 -0500

    Battery thresholds

--- modules/nixos/desktop/tlp/default.nix
+++ modules/nixos/desktop/tlp/default.nix
@@ -3,9 +3,26 @@
   lib,
   ...
 }: let
-  inherit (lib) mkIf mkOption types;
+  inherit (lib) mkIf mkMerge mkOption types;
 
   cfg = config.modulo.desktop.tlp;
+
+  batteryThresholds = {
+    preferBattery = {
+      start = 95;
+      stop = 100;
+    };
+
+    preferBalanced = {
+      start = 75;
+      stop = 80;
+    };
+
+    preferAC = {
+      start = 45;
+      stop = 50;
+    };
+  };
 in {
   options.modulo.desktop.tlp = {
     cpu = mkOption {
@@ -32,17 +49,40 @@ in {
         TLP CPU governor configuration.
       '';
     };
+
+    battery = {
+      chargeMode = mkOption {
+        type = with types;
+          nullOr (enum [
+            "preferBattery"
+            "preferBalanced"
+            "preferAC"
+          ]);
+        default = null;
+        description = ''
+          Battery charging thresholds configuration.
+          Pass `null` to disable battery thresholds entirely.
+        '';
+      };
+    };
   };
 
   config.services.tlp = mkIf (cfg.cpu != null) {
     enable = true;
 
-    settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = cfg.cpu.ac;
-      CPU_SCALING_GOVERNOR_ON_BAT = cfg.cpu.bat;
+    settings = mkMerge [
+      {
+        CPU_SCALING_GOVERNOR_ON_AC = cfg.cpu.ac;
+        CPU_SCALING_GOVERNOR_ON_BAT = cfg.cpu.bat;
 
-      CPU_BOOST_ON_AC = 1;
-      CPU_BOOST_ON_BAT = 0;
-    };
+        CPU_BOOST_ON_AC = 1;
+        CPU_BOOST_ON_BAT = 0;
+      }
+
+      (mkIf (cfg.battery.chargeMode != null) {
+        START_CHARGE_THRESH_BAT0 = batteryThresholds.${cfg.battery.chargeMode}.start;
+        STOP_CHARGE_THRESH_BAT0 = batteryThresholds.${cfg.battery.chargeMode}.stop;
+      })
+    ];
   };
 }
