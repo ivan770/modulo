37546c48ffdb3333a78cbf3fade4ff49b61f82f5
ivan770 <ivan@ivan770.me>
2025-06-14 17:19:21 +0000

    Move rnnoise to packages

--- modules/home/desktop/sound/default.nix
+++ modules/home/desktop/sound/default.nix
@@ -13,34 +13,10 @@ let
     types
     ;
 
+  inherit (pkgs.modulo) rnnoise;
+
   cfg = config.modulo.desktop.sound;
-
   soundEnabled = osConfig.modulo.desktop.sound.enable;
-
-  rnnoise = pkgs.rnnoise-plugin.overrideAttrs (old: {
-    cmakeFlags = [
-      "-DUSE_SYSTEM_JUCE=ON"
-      "-DBUILD_FOR_RELEASE=ON"
-      "-DBUILD_VST_PLUGIN=OFF"
-      "-DBUILD_VST3_PLUGIN=OFF"
-      "-DBUILD_LV2_PLUGIN=OFF"
-    ];
-
-    outputs = [ "out" ];
-
-    buildInputs = (old.buildInputs or [ ]) ++ [ pkgs.juce ];
-
-    # rnnoise README file recommends using target-specific optimizations.
-    # These flags are somewhat arbitrary though.
-    NIX_CFLAGS_COMPILE = (old.NIX_CFLAGS_COMPILE or [ ]) ++ [
-      "-march=znver3"
-      "-mtune=znver4"
-    ];
-
-    # Default postInstall includes separate outputs for plugins,
-    # but since we build one plugin in the first place, just rely on $out.
-    postInstall = "";
-  });
 in
 {
   options.modulo.desktop.sound = {
